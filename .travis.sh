d953b319277366f99a57c265307b1734ddce0eff
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2015-11-25 14:52:30 +0900

    add pkg-config to dependency

--- .travis.sh
+++ .travis.sh
@@ -4,7 +4,7 @@ set -x
 
 sudo add-apt-repository -y ppa:hrg/daily
 sudo apt-get update -qq
-sudo apt-get install -qq -y choreonoid libcnoid-dev libsdformat-dev
+sudo apt-get install -qq -y pkg-config choreonoid libcnoid-dev libsdformat-dev
 
 cmake .
 make
